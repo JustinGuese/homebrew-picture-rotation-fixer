9e4744a2a7bf890cd902fecf013812a118d5be8a
Justin Guese <guese.justin@gmail.com>
2025-08-28 09:49:26 +0200

    v0.0.6

--- Formula/picture-rotation-fixer.rb
+++ Formula/picture-rotation-fixer.rb
@@ -1,17 +1,17 @@
 class PictureRotationFixer < Formula
   desc "Effortlessly auto-rotate scanned photos to the correct orientation"
   homepage "https://github.com/JustinGuese/picture-rotation-fixer"
-  version "0.0.5"
+  version "0.0.6"
 
   on_macos do
     on_intel do
-      url "https://github.com/JustinGuese/picture-rotation-fixer/releases/download/v0.0.5/picture-rotation-fixer-macos-x64"
-      sha256 "e833230110d5bd12cdeac36222f04cf6b377740811db7feee7f72c491d51313a"
+      url "https://github.com/JustinGuese/picture-rotation-fixer/releases/download/v0.0.6/picture-rotation-fixer-macos-x64"
+      sha256 "b57fe78f98fe9316fbe12b4dc44f98977d7c4923a6886df252d1e888f9365dac"
     end
 
     on_arm do
-      url "https://github.com/JustinGuese/picture-rotation-fixer/releases/download/v0.0.5/picture-rotation-fixer-macos-arm64"
-      sha256 "5a04d93eb90aec3b3ba24f1fb377c7941c205b50c55c534ed7f20de763db1aa7"
+      url "https://github.com/JustinGuese/picture-rotation-fixer/releases/download/v0.0.6/picture-rotation-fixer-macos-arm64"
+      sha256 "97f48c88cf9ddf35cfbe9c5fc93fadf33193fa8c70241b09435390678a3eb76e"
     end
   end
 

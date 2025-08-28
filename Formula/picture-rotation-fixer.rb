841919404c543f6cd5f29021e2bfdefd9982cf99
Justin Guese <guese.justin@gmail.com>
2025-08-28 09:18:45 +0200

    new sha

--- Formula/picture-rotation-fixer.rb
+++ Formula/picture-rotation-fixer.rb
@@ -2,7 +2,7 @@ class PictureRotationFixer < Formula
   desc "Effortlessly auto-rotate scanned photos to the correct orientation"
   homepage "https://github.com/JustinGuese/picture-rotation-fixer"
   url "https://github.com/JustinGuese/picture-rotation-fixer/releases/download/v0.0.3/picture-rotation-fixer"
-  sha256 "cf3ba56171899864ad1c5c47d15786482a4e2081cc820670dec3cba9913708a8"
+  sha256 "4c775c88b2bf12d323970382c1af4a6aa7caf355b5df90b92b787e877f5e19a2"
 
   def install
     bin.install "picture-rotation-fixer"

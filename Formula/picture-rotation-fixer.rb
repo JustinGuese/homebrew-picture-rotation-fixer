bcdb8b4e392e66d7a16671a6757ee2bb67055820
Justin Guese <guese.justin@gmail.com>
2025-08-28 09:32:22 +0200

    arm64 support

--- Formula/picture-rotation-fixer.rb
+++ Formula/picture-rotation-fixer.rb
@@ -1,12 +1,27 @@
 class PictureRotationFixer < Formula
   desc "Effortlessly auto-rotate scanned photos to the correct orientation"
   homepage "https://github.com/JustinGuese/picture-rotation-fixer"
-  url "https://github.com/JustinGuese/picture-rotation-fixer/releases/download/v0.0.3/picture-rotation-fixer"
-  sha256 "4c775c88b2bf12d323970382c1af4a6aa7caf355b5df90b92b787e877f5e19a2"
+  version "0.0.4"
+
+  on_macos do
+    on_intel do
+      url "https://github.com/JustinGuese/picture-rotation-fixer/releases/download/v0.0.4/picture-rotation-fixer-macos-x64"
+      sha256 "db203176841ebfc5b3babbd2e9de12d677454c5dc606472f4902079f74eeb8bb"
+    end
+
+    on_arm do
+      url "https://github.com/JustinGuese/picture-rotation-fixer/releases/download/v0.0.4/picture-rotation-fixer-macos-arm64"
+      sha256 "f24f1149358361b80568b845858ee5ae308942fd9e85191b9f33842d38c16dd1"
+    end
+  end
 
   def install
-    chmod 0755, "picture-rotation-fixer"
-    bin.install "picture-rotation-fixer"
+    if Hardware::CPU.intel?
+      bin.install "picture-rotation-fixer-macos-x64" => "picture-rotation-fixer"
+    else
+      bin.install "picture-rotation-fixer-macos-arm64" => "picture-rotation-fixer"
+    end
+    chmod 0755, bin/"picture-rotation-fixer"
   end
 
   test do
